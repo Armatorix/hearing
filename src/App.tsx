0ccd905e5cac05258af87e91de13d1de0a1f8159
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2023-02-22 01:31:29 +0100

    side picker switch

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import { useFrequency } from 'react-frequency';
 import Hearing from './Hearing';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { Switch } from '@mui/material';
 
 
 function App() {
+  const [side, setSide] = useState("left" as "left" | "right")
   return (
     <div className="App">
-      <Hearing freq={2137} />
+      <FormControlLabel
+        value={side}
+        control={<Switch color="primary" />}
+        label={side}
+        labelPlacement="bottom"
+        onClick={() => {
+          setSide((old) => old == "left" ? "right" : "left")
+        }}
+      />
+      <Hearing
+        freq={7137}
+        side={side}
+      />
     </div >
   );
 }
