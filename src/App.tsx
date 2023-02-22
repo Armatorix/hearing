3513ba40cfc1988ce56f4ae0eac85b421c5a225a
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2023-02-22 17:21:36 +0100

    baseline with dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { Switch } from '@mui/material';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { useState } from 'react';
-import './App.css';
-import Hearing from './Hearing';
-
+import { Switch } from '@mui/material'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import { useState } from 'react'
+import './App.css'
+import Hearing from './Hearing'
 
 function App() {
-  const [side, setSide] = useState("left" as "left" | "right")
+  const [side, setSide] = useState('left' as 'left' | 'right')
   return (
     <div className="App">
       <FormControlLabel
@@ -15,16 +14,12 @@ function App() {
         label={side}
         labelPlacement="bottom"
         onClick={() => {
-          setSide((old) => old === "left" ? "right" : "left")
+          setSide((old) => (old === 'left' ? 'right' : 'left'))
         }}
       />
-      <Hearing
-        freq={7137}
-        side={side}
-      />
-    </div >
-  );
+      <Hearing freq={7137} side={side} />
+    </div>
+  )
 }
 
-export default App;
- 
+export default App
--- src/Hearing.tsx
+++ src/Hearing.tsx
@@ -1,38 +1,36 @@
-import Button from "@mui/material/Button";
-import { useEffect, useState } from "react";
-import { useFrequency } from "react-frequency";
+import Button from '@mui/material/Button'
+import { useEffect, useState } from 'react'
+import { useFrequency } from 'react-frequency'
 
-const Hearing = (props:
-    {
-        freq: number,
-        side: "right" | "left",
+const Hearing = (props: { freq: number; side: 'right' | 'left' }) => {
+  const [gain, setGain] = useState(0)
+  const { toggle } = useFrequency({
+    hz: props.freq,
+    type: props.side,
+    oscillator: 'sine',
+    gain: gain / 100,
+  })
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setGain((g) => Math.min(g + 10, 100))
+    }, 1000)
+
+    return () => {
+      clearInterval(id)
     }
-) => {
-    const [gain, setGain] = useState(0);
-    const { toggle } = useFrequency({
-        hz: props.freq,
-        type: props.side,
-        oscillator: "sine",
-        gain: gain / 100,
-    });
-
-    useEffect(() => {
-        const id = setInterval(() => {
-            setGain((g) => Math.min(g + 10, 100))
-        }, 1000
-        );
-
-        return () => {
-            clearInterval(id);
-        };
-    }, []);
-    return (
-        <Button onClick={() => {
-            setGain(0);
-            setTimeout(() => {
-                toggle();
-            }, 150)
-        }}>I hear!</Button>
-    );
+  }, [])
+  return (
+    <Button
+      onClick={() => {
+        setGain(0)
+        setTimeout(() => {
+          toggle()
+        }, 150)
+      }}
+    >
+      I hear!
+    </Button>
+  )
 }
-export default Hearing;
+export default Hearing
