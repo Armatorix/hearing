d719040ba13d4bd52aa9f49615776d8321e01692
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2023-02-22 18:21:11 +0100

    app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch } from '@mui/material'
+import { AppBar, Container, Switch, Toolbar, Typography } from '@mui/material'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import { useState } from 'react'
 import './App.css'
@@ -7,18 +7,25 @@ import Hearing from './Hearing'
 function App() {
   const [side, setSide] = useState('left' as 'left' | 'right')
   return (
-    <div className="App">
-      <FormControlLabel
-        value={side}
-        control={<Switch color="primary" />}
-        label={side}
-        labelPlacement="bottom"
-        onClick={() => {
-          setSide((old) => (old === 'left' ? 'right' : 'left'))
-        }}
-      />
-      <Hearing freq={7137} side={side} />
-    </div>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4">Hearing monitor</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className="App" maxWidth="sm">
+        <FormControlLabel
+          value={side}
+          control={<Switch color="primary" />}
+          label={side}
+          labelPlacement="bottom"
+          onClick={() => {
+            setSide((old) => (old === 'left' ? 'right' : 'left'))
+          }}
+        />
+        <Hearing freq={7137} side={side} />
+      </Container>
+    </>
   )
 }
 
