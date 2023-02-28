5e56f6631ecd911b645600a2c97162a6d3dabbdc
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2023-02-28 14:08:14 +0100

    with tooltip

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,12 +1,13 @@
-import { Typography } from '@mui/material'
+import { IconButton, Tooltip, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
+import HelpIcon from '@mui/icons-material/Help'
 import {
   CartesianGrid,
   Legend,
   Line,
   LineChart,
   ResponsiveContainer,
-  Tooltip,
+  Tooltip as ChartTooltip,
   XAxis,
   YAxis,
 } from 'recharts'
@@ -20,13 +21,23 @@ const Results = (props: { data: Datapoint[] }) => {
   const { innerWidth: width, innerHeight: height } = window
   return (
     <Stack spacing={4}>
-      <Typography variant="h5">Results</Typography>
+      <Typography variant="h5">
+        Results
+        <Tooltip
+          title="Lower value is better. Less volume was needed for you to hear the noice."
+          disableInteractive
+        >
+          <IconButton>
+            <HelpIcon />
+          </IconButton>
+        </Tooltip>
+      </Typography>
       <ResponsiveContainer width="100%" height={height / 2}>
         <LineChart data={props.data}>
           <XAxis dataKey="name" unit="Hz" name="Frequency" />
           <YAxis unit="%" reversed />
           <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-          <Tooltip label="name" />
+          <ChartTooltip label="name" />
           <Legend />
           <Line type="monotone" dataKey="right" stroke="#8884d8" />
           <Line type="monotone" dataKey="left" stroke="#82ca9d" />
